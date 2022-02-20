9040525470ba8d3eb442b32040748b1997875638
Pawel-Cwik <pawelcwik99@gmail.com>
2022-02-20 01:22:07 +0100

    Try1
    sprawdzenie istnienia folderów i wstępne przydzielenia

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("PAWEL")
+import numpy as np
+import argparse
+import cv2
+
+import xml.etree.ElementTree as ET
+import os
+import shutil
+
+if os.path.exists("train"):
+    print("Istnieje")
+else:
+    os.mkdir("train")
+if os.path.exists("test"):
+    print("Istnieje")
+else:
+    os.mkdir("test")
+
+
+path = "../annotations"
+for filename in os.listdir(path):
+    if not filename.endswith('.xml'): continue
+    fullname = os.path.join(path, filename)
+    tree = ET.parse(fullname)
+    root = tree.getroot()
+    for child in root.findall('object'):
+         name= child.find('name').text
+         if name == 'speedlimit':
+             shutil.copy(f'../annotations/{filename}', "./test")
+#             print(True)
+
+
+
+
+
+
+
+
+
